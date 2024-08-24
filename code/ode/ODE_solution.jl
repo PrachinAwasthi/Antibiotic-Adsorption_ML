c84c2b5fb2d1dd8373de2c5802cfad4df014ad27
Prachi Awasthi <nainsiawasthi@gmail.com>
2024-08-24 18:54:32 +0530

    Add files via upload

---
+++ code/ode/ODE_solution.jl
@@ -0,0 +1,44 @@
+##load the packages
+using DifferentialEquations, Plots
+using LinearAlgebra, Statistics, StableRNGs
+##define variables and parameters
+#α=ka, β=kb, γ=C, δ=Qmax
+α=(0.01)
+β=(0.001)
+γ=5.0
+δ=100.0
+p=[α, β, γ, δ]
+#define equation in terms of function
+
+function ODE(du,u,p,t)
+   du[1]= α*γ*(δ-u[1])-β*u[1]
+end
+
+#define initial values
+u0=[0.0]
+p0=[0.01, 0.001, 5.0, 100.0]
+tspan=(0.0, 350.0)
+datasize=50
+tsteps=range(tspan[1], tspan[2], length=datasize)
+
+## call ODEProblem function 
+prob= ODEProblem(ODE, u0, tspan, p0)
+
+## solve the problem
+sol = solve(prob, Tsit5(), saveat=tsteps)
+
+#analyse the result 
+true_sol=Array(sol)
+t=sol.t
+
+###PLot  the results
+using Plots.PlotMeasures
+plot_size = (1200, 600)
+left_margin = 20px
+right_margin = 20px
+top_margin = 20px
+bottom_margin = 20px
+
+true_solution=plot(t, true_sol[1, :], seriestype=:line, lw=3, color=:red, alpha=0.4, label="True Solution", xlabel="Time (min)", ylabel="Adsorption capacity (mg/g)", title="Langmuir Adsorption Model", size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright)
+
+png("true_solution")
