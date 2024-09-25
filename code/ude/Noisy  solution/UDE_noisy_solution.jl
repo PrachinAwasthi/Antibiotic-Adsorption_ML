4a8fc37ac85e545b833651055005929780807e25
Prachi Awasthi <nainsiawasthi@gmail.com>
2024-09-25 10:42:13 +0530

    Add files via upload

--- code/ude/Noisy  solution/UDE_noisy_solution.jl
+++ code/ude/Noisy  solution/UDE_noisy_solution.jl
@@ -120,10 +120,13 @@ right_margin = 20px
 top_margin = 20px
 bottom_margin = 20px
 
-plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],   seriestype=:line, linestyle=:dash, lw=5, color=:brown, label="True data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
-plot!(ts, transpose(nn_ude_pred_noisy_sol),  seriestype=:line, lw=3, label="UDE predicted data", color=:lightseagreen)
-savefig("low noise_true noisy data_predicted UDE.png")
-savefig("low noise_true noisy data_predicted UDE.svg")
+#plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],   seriestype=:line, linestyle=:dash, lw=5, color=:brown, label="True data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+#plot!(ts, transpose(nn_ude_pred_noisy_sol),  seriestype=:line, lw=3, label="UDE predicted data", color=:lightseagreen)
+#savefig("low noise_true noisy data_predicted UDE.png")
+#savefig("low noise_true noisy data_predicted UDE.svg")
+plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],  seriestype=:scatter, marker=:circle,markersize=7.0, alpha=0.5, color=:blue, label="Training data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+plot!(ts, transpose(nn_ude_pred_noisy_sol),  seriestype=:line, lw=3, label="Predicted data", color=:black)
+savefig("2_low noise_true noisy data_predicted UDE.png")
 
 
 # Compute the true interactions 
@@ -133,16 +136,25 @@ Ȳ=true_missing_terms
 nn_pred_missing_terms = nn(nn_ude_pred_noisy_sol, p_final, st)[1]
 Ŷ=nn_pred_missing_terms
 # Plot the true missing terms and the UDE approximations
-plot(t, true_missing_terms',seriestype=:scatter, marker=:circle, markersize=7.0, color=:magenta, alpha=0.8,  label="Actual missing term", xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="Missing Desorption Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:topright, grid=false, legendfontsize=14)
-plot_reconstruction = plot!(t, nn_pred_missing_terms', seriestype=:line, lw=5, color=:cyan, label="UDE Predicted missing term")
+#plot(t, true_missing_terms',seriestype=:scatter, marker=:circle, markersize=7.0, color=:magenta, alpha=0.8,  label="Actual missing term", xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="Missing Desorption Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:topright, grid=false, legendfontsize=14)
+#plot_reconstruction = plot!(t, nn_pred_missing_terms', seriestype=:line, lw=5, color=:cyan, label="UDE Predicted missing term")
+#savefig("true noisy missing term_UDE prediction.png")
+#savefig("true noisy missing term_UDE prediction.svg")
 
-savefig("true noisy missing term_UDE prediction.png")
-savefig("true noisy missing term_UDE prediction.svg")
+plot(t, true_missing_terms',marker=:circle,seriestype=:scatter,markersize=7.0, alpha=0.8, color=:red, label="Actual term",xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="UDE Missing Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:topright, grid=false, legendfontsize=14)
+plot_reconstruction = plot!(t, nn_pred_missing_terms',seriestype=:line, lw=3, color=:seagreen, label="UDE Approximation")
+
+savefig("2_true noisy missing term_UDE prediction.png")
+
 #plot the error
-plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error Plot",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black, label = nothing, color = :seagreen, lw=3,  legendfontsize=14)
+#plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error Plot",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black, label = nothing, color = :seagreen, lw=3,  legendfontsize=14)
 
-savefig("error.png")
-savefig("error.svg")
+#savefig("error.png")
+#savefig("error.svg")
+
+plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black,  label = nothing, color = :orange, lw=3,  legendfontsize=14)
+savefig("2_error.png")
+
 #all in one
 using Plots.PlotMeasures
 plot_size = (1200, 800)
@@ -151,11 +163,12 @@ right_margin = 15px
 top_margin = 15px
 bottom_margin = 15px
 
+#plot_overall = plot(size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, plot_true_noisy_UDE_approximation, plot_reconstruction, plot_error)
+#savefig("true noisy_UDE_missing_terms_error.png")
+#savefig("true noisy_UDE_missing_terms_error.svg")
+#savefig("true noisy_UDE_missing_terms_error.pdf")
 plot_overall = plot(size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, plot_true_noisy_UDE_approximation, plot_reconstruction, plot_error)
-savefig("true noisy_UDE_missing_terms_error.png")
-savefig("true noisy_UDE_missing_terms_error.svg")
-savefig("true noisy_UDE_missing_terms_error.pdf")
-
+savefig("2_true noisy_UDE_missing_terms_error.png")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`High   noise  solution-  5e-2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##load the packages
@@ -279,10 +292,15 @@ right_margin = 20px
 top_margin = 20px
 bottom_margin = 20px
 
-plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],   seriestype=:line, linestyle=:dash, lw=5, color=:brown, label="True data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
-plot!(ts, transpose(nn_ude_pred_high_noisy_sol),  seriestype=:line, lw=3, label="UDE predicted data", color=:lightseagreen)
-savefig("high noise_true noisy data_predicted UDE.png")
-savefig("high noise_true noisy data_predicted UDE.svg")
+#plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],   seriestype=:line, linestyle=:dash, lw=5, color=:brown, label="True data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+#plot!(ts, transpose(nn_ude_pred_high_noisy_sol),  seriestype=:line, lw=3, label="UDE predicted data", color=:lightseagreen)
+#savefig("high noise_true noisy data_predicted UDE.png")
+#savefig("high noise_true noisy data_predicted UDE.svg")
+plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :], seriestype=:scatter, marker=:circle,markersize=7.0, alpha=0.5, color=:blue, label="Training data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+plot!(ts, transpose(nn_ude_pred_high_noisy_sol),seriestype=:line, lw=3, label="Predicted data", color=:black)
+savefig("2_high noise_true noisy data_predicted UDE.png")
+
+
 
 
 # Compute the true interactions 
@@ -292,16 +310,22 @@ Ȳ=true_missing_terms
 nn_pred_missing_terms = nn(nn_ude_pred_high_noisy_sol, p_final, st)[1]
 Ŷ=nn_pred_missing_terms
 # Plot the true missing terms and the UDE approximations
-plot(t, true_missing_terms',seriestype=:scatter, marker=:circle, markersize=7.0, color=:magenta, alpha=0.8,  label="Actual missing term", xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="Missing Desorption Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
-plot_reconstruction = plot!(t, nn_pred_missing_terms', seriestype=:line, lw=5, color=:cyan, label="UDE Predicted missing term")
+#plot(t, true_missing_terms',seriestype=:scatter, marker=:circle, markersize=7.0, color=:magenta, alpha=0.8,  label="Actual missing term", xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="Missing Desorption Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+#plot_reconstruction = plot!(t, nn_pred_missing_terms', seriestype=:line, lw=5, color=:cyan, label="UDE Predicted missing term")
+#savefig("high noise_true noisy missing term_UDE prediction.png")
+#savefig("high noise_true noisy missing term_UDE prediction.svg")
 
-savefig("high noise_true noisy missing term_UDE prediction.png")
-savefig("high noise_true noisy missing term_UDE prediction.svg")
+plot(t, true_missing_terms',seriestype=:scatter, marker=:circle,markersize=7.0, alpha=0.8, color=:red, label="Actual term",xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="UDE Missing Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+plot_reconstruction = plot!(t, nn_pred_missing_terms',  seriestype=:line, lw=3, color=:seagreen, label="UDE Approximation")
+
+savefig("2_high noise_true noisy missing term_UDE prediction.png")
 #plot the error
-plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error Plot",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black, label = nothing, color = :seagreen, lw=3,  legendfontsize=14)
+#plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error Plot",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black, label = nothing, color = :seagreen, lw=3,  legendfontsize=14)
+#savefig("high error.png")
+#savefig("high error.svg")
 
-savefig("high error.png")
-savefig("high error.svg")
+plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black,  label = nothing, color = :orange, lw=3,  legendfontsize=14)
+savefig("2_high error.png")
 #all in one
 using Plots.PlotMeasures
 plot_size = (1200, 800)
@@ -311,9 +335,9 @@ top_margin = 15px
 bottom_margin = 15px
 
 plot_overall = plot(size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, plot_true_noisy_UDE_approximation, plot_reconstruction, plot_error)
-savefig("high noise_true noisy_UDE_missing_terms_error.png")
-savefig("high noise_true noisy_UDE_missing_terms_error.svg")
-savefig("high noise_true noisy_UDE_missing_terms_error.pdf")
+savefig("2_high noise_true noisy_UDE_missing_terms_error.png")
+#savefig("high noise_true noisy_UDE_missing_terms_error.svg")
+#savefig("high noise_true noisy_UDE_missing_terms_error.pdf")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`Very High   noise  solution-  5e-1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##load the packages
@@ -437,11 +461,13 @@ right_margin = 20px
 top_margin = 20px
 bottom_margin = 20px
 
-plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],   seriestype=:line, linestyle=:dash, lw=5, color=:brown, label="True data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
-plot!(ts, transpose(nn_ude_pred_very_high_noisy_sol),  seriestype=:line, lw=3, label="UDE predicted data", color=:lightseagreen)
-savefig("very high noise_true noisy data_predicted UDE.png")
-savefig("very high noise_true noisy data_predicted UDE.svg")
-
+#plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :],   seriestype=:line, linestyle=:dash, lw=5, color=:brown, label="True data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+#plot!(ts, transpose(nn_ude_pred_very_high_noisy_sol),  seriestype=:line, lw=3, label="UDE predicted data", color=:lightseagreen)
+#savefig("very high noise_true noisy data_predicted UDE.png")
+#savefig("very high noise_true noisy data_predicted UDE.svg")
+plot_true_noisy_UDE_approximation=plot(t, true_sol_noisy[1, :], seriestype=:scatter, marker=:circle,markersize=7.0, alpha=0.5, color=:blue, label="Training data", xlabel="Time (min)", ylabel="Adsorption Capacity (mg/g)", labelfontsize=14, labelcolor=:darkblack, title="Langmuir Adsorption", titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+plot!(ts, transpose(nn_ude_pred_very_high_noisy_sol),  seriestype=:line, lw=3, label="Predicted data", color=:black)
+savefig("2_very high noise_true noisy data_predicted UDE.png")
 
 # Compute the true interactions 
 true_missing_terms=p_initial[2].* (nn_ude_pred_very_high_noisy_sol[1, :])'
@@ -450,16 +476,22 @@ Ȳ=true_missing_terms
 nn_pred_missing_terms = nn(nn_ude_pred_very_high_noisy_sol, p_final, st)[1]
 Ŷ=nn_pred_missing_terms
 # Plot the true missing terms and the UDE approximations
-plot(t, true_missing_terms',seriestype=:scatter, marker=:circle, markersize=7.0, color=:magenta, alpha=0.8,  label="Actual missing term", xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="Missing Desorption Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
-plot_reconstruction = plot!(t, nn_pred_missing_terms', seriestype=:line, lw=5, color=:cyan, label="UDE Predicted missing term")
+#plot(t, true_missing_terms',seriestype=:scatter, marker=:circle, markersize=7.0, color=:magenta, alpha=0.8,  label="Actual missing term", xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="Missing Desorption Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+#plot_reconstruction = plot!(t, nn_pred_missing_terms', seriestype=:line, lw=5, color=:cyan, label="UDE Predicted missing term")
 
-savefig("very high noise_true noisy missing term_UDE prediction.png")
-savefig("very high noise_true noisy missing term_UDE prediction.svg")
+#savefig("very high noise_true noisy missing term_UDE prediction.png")
+#savefig("very high noise_true noisy missing term_UDE prediction.svg")
+
+plot(t, true_missing_terms',seriestype=:scatter,marker=:circle,markersize=7.0, alpha=0.8, color=:red, label="Actual term",xlabel="Time (min)", ylabel="Desorption Rate (mg/g min)", labelfontsize=14, labelcolor=:black, title="UDE Missing Term",titlefontsize=16, size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, legend=:bottomright, grid=false, legendfontsize=14)
+plot_reconstruction = plot!(t, nn_pred_missing_terms',  seriestype=:line, lw=3, color=:seagreen, label="UDE Approximation")
+savefig("2_very high noise_true noisy missing term_UDE prediction.png")
 #plot the error
-plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error Plot",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black, label = nothing, color = :seagreen, lw=3,  legendfontsize=14)
+#plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error Plot",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black, label = nothing, color = :seagreen, lw=3,  legendfontsize=14)
+#savefig("very high error.png")
+#savefig("very high error.svg")
 
-savefig("very high error.png")
-savefig("very high error.svg")
+plot_error=plot(t, norm.(eachcol(Ȳ - Ŷ)), yaxis = :log, title="Error",  xlabel = "Time (min)", ylabel = "Error", labelfontsize=12, labelcolor=:black,  label = nothing, color = :orange, lw=3,  legendfontsize=14)
+savefig("2_very high error.png")
 #all in one
 using Plots.PlotMeasures
 plot_size = (1200, 800)
@@ -469,6 +501,7 @@ top_margin = 15px
 bottom_margin = 15px
 
 plot_overall = plot(size=plot_size, left_margin=left_margin, right_margin=right_margin, bottom_margin=bottom_margin, top_margin=top_margin, plot_true_noisy_UDE_approximation, plot_reconstruction, plot_error)
-savefig("very high noise_true noisy_UDE_missing_terms_error.png")
-savefig("very high noise_true noisy_UDE_missing_terms_error.svg")
-savefig("very high noise_true noisy_UDE_missing_terms_error.pdf")
+#savefig("very high noise_true noisy_UDE_missing_terms_error.png")
+#savefig("very high noise_true noisy_UDE_missing_terms_error.svg")
+#savefig("very high noise_true noisy_UDE_missing_terms_error.pdf")
+savefig("2_very high noise_true noisy_UDE_missing_terms_error.png")
